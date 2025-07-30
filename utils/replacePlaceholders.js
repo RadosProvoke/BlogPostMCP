46a260aedab1f040c8a7df0dc26120181e1de38e
RadosProvoke <radosr@provokesolutions.com>
2025-07-30 12:50:20 +0200

    Update replacePlaceholders.js

--- utils/replacePlaceholders.js
+++ utils/replacePlaceholders.js
@@ -1,7 +1,21 @@
+/*
 const createReport = require('docx-templates').default;
 
 console.log('createReport:', createReport);
 
+
+async function replacePlaceholders(templateBuffer, data) {
+  return await createReport({
+    template: templateBuffer,
+    data,
+  });
+}
+
+module.exports = { replacePlaceholders };
+*/
+const docxTemplates = require('docx-templates');
+
+const createReport = docxTemplates.default || docxTemplates;
 
 async function replacePlaceholders(templateBuffer, data) {
   return await createReport({
