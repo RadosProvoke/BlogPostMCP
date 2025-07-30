745fafac17377270d0cae21389859d3ad2725d54
RadosProvoke <radosr@provokesolutions.com>
2025-07-30 18:13:49 +0200

    Update docxGenerator.js

--- services/docxGenerator.js
+++ services/docxGenerator.js
@@ -26,6 +26,7 @@ const { replacePlaceholders } = require('../utils/replacePlaceholders');
 
 async function createDocx({ title, body }) {
   const templatePath = path.join(__dirname, '..', 'templates', 'blog_template.docx');
+  console.log(templatePath);
   const templateBuffer = fs.readFileSync(templatePath);
   const filledBuffer = await replacePlaceholders(templateBuffer, { TITLE: title, CONTENT: body });
   return filledBuffer;
