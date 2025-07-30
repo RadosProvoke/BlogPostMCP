c333e7cf1cdbfa3e7c37d59d35a10ca60499df40
RadosProvoke <radosr@provokesolutions.com>
2025-07-30 13:37:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,32 +2,34 @@ const express = require('express');
 const multer = require('multer');
 const dotenv = require('dotenv');
 const { generateBlogContent } = require('./services/openai');
-//const { createDocxFromBuffer } = require('./services/docxGenerator');
-console.log("Blog content:", blogContent);
-const docBuffer = await createDocx(blogContent);
 const { createDocx } = require('./services/docxGenerator');
-//const { uploadToSharePoint, downloadTemplate } = require('./services/sharepoint');
 const { uploadToBlob } = require('./services/storage');
 const { extractTextFromTranscript } = require('./utils/parseTranscript');
 
 dotenv.config();
+
 const app = express();
 const upload = multer();
 
 app.post('/generate-blogpost', upload.single('transcript'), async (req, res) => {
   try {
-    
     const buffer = req.file.buffer;
     const filename = req.file.originalname;
     const transcript = extractTextFromTranscript(buffer, filename);
 
+    // generating blog post content
     const blogContent = await generateBlogContent(transcript);
+    console.log("Blog content:", blogContent); // blog content
+
+    // creating .docx file with field names 
     const docBuffer = await createDocx({ title: blogContent.title, body: blogContent.content });
 
+    // Upload to Blob storage
     const blobURL = await uploadToBlob(docBuffer, blogContent.title);
+
+    // sending URL-a to the client
     res.json({ title: blogContent.title, url: blobURL });
 
-    
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: err.message, stack: err.stack });
